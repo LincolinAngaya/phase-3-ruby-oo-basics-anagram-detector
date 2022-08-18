fb9826dd6a4e19326b8993595c509b63156c07de
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-08-18 17:04:04 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,25 +1,4 @@
-# # Your code goes here!
-#  class Anagram 
-#     attr_accessor :word 
 
-#     def initialize(word)
-#       @Word = word
-#     end
-
-#   def match(word_array)
-#         word_array.select {|x| x.split("").sort == @word.split("").sort}
-#     end
-#  end 
-
-#class Anagram 
-#     def initialize(word)
-#       @Word = word
-#     end
-
-#     def match(word_array)
-
-#     end
-# end
  class Anagram
      attr_accessor :name 
 
