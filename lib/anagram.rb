707c4c1d75cf7bb0204feff654756f563b50bab5
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-08-18 17:03:24 +0300

    commit tree 978ab60d3b84f4492c117d7df7281117718d9620

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
-# Your code goes here!
+# # Your code goes here!
+#  class Anagram 
+#     attr_accessor :word 
+
+#     def initialize(word)
+#       @Word = word
+#     end
+
+#   def match(word_array)
+#         word_array.select {|x| x.split("").sort == @word.split("").sort}
+#     end
+#  end 
+
+#class Anagram 
+#     def initialize(word)
+#       @Word = word
+#     end
+
+#     def match(word_array)
+
+#     end
+# end
+ class Anagram
+     attr_accessor :name 
+
+    def initialize(given_word)
+       @name = given_word 
+     end   
+
+     def match(my_array)
+       my_array.select {|x| x.split("").sort == @name.split("").sort}
+     end   
+  end
